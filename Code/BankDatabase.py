922eecb41ffe3e33eb817ad7bca619a98d38e597
mustapha GHIATI <160877694+ghiati@users.noreply.github.com>
2024-03-14 23:07:26 +0100

    Update BankDatabase.py

--- Code/BankDatabase.py
+++ Code/BankDatabase.py
@@ -1,6 +1,6 @@
-from pymongo import MongoClient
+from pymongo import MongoClient #nous avons besoin mosieur de Pymongo pour faire la connection entre python et mongo db 
 
-# Connect to MongoDB
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client['bank_system']  
 users_collection = db['users'] 

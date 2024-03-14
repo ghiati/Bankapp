3d048f21cf53bc57f64bc4cd53865a828ce16237
mustapha GHIATI <160877694+ghiati@users.noreply.github.com>
2024-03-14 23:04:32 +0100

    Add files via upload

---
+++ Code/BankDatabase.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['bank_system']  
+users_collection = db['users'] 
+historique_collection = db['historique']  

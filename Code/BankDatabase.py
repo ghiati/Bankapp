f6eba2c7949048cf390041868a170c2b8d82f681
mustapha GHIATI <160877694+ghiati@users.noreply.github.com>
2024-03-15 03:32:54 +0100

    Delete Code/BankDatabase.py

--- Code/BankDatabase.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient #on va utilise mongo db pour stoque les information de l'utilisateur 
-
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client['bank_system']  
-users_collection = db['users'] 
-historique_collection = db['historique']  

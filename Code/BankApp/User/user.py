eb5023b3257b148d996a43ccb3b0e3bf4fe3ea06
mustapha GHIATI <160877694+ghiati@users.noreply.github.com>
2024-03-15 04:18:57 +0100

    Update user.py

--- Code/BankApp/User/user.py
+++ Code/BankApp/User/user.py
@@ -6,7 +6,7 @@ class CompteUtilisateur:
         self.nom = nom
         self.email = email
         self.mot_de_passe = mot_de_passe
-        self.solde = solde
+        self.solde = float(solde)
         self.rib = self.generer_rib()
 
     def generer_rib(self):
@@ -14,27 +14,39 @@ class CompteUtilisateur:
         return rib
 
     def deposer(self, montant):
-        if montant > 0:
-            self.solde += montant
-            print(f"{montant} déposés dans le compte de {self.prenom}.")
-            self.afficher_solde()
-        else:
-            print("Montant de dépôt non valide.")
+        try:
+            montant = float(montant)  # Convert input to float
+            if montant > 0:
+                self.solde += montant
+                print(f"{montant} déposés dans le compte de {self.prenom}.")
+                self.afficher_solde()
+            else:
+                print("Montant de dépôt non valide.")
+        except ValueError:
+            print("Montant de dépôt invalide. Veuillez entrer un nombre.")
 
     def retirer(self, montant):
-        if 0 < montant <= self.solde:
-            self.solde -= montant
-            print(f"{montant} retirés du compte de {self.prenom}.")
-            self.afficher_solde()
-        else:
-            print("Fonds insuffisants ou montant non valide.")
+        try:
+            montant = float(montant)  # Convert input to float
+            if 0 < montant <= self.solde:
+                self.solde -= montant
+                print(f"{montant} retirés du compte de {self.prenom}.")
+                self.afficher_solde()
+            else:
+                print("Fonds insuffisants ou montant non valide.")
+        except ValueError:
+            print("Montant de retrait invalide. Veuillez entrer un nombre.")
 
     def transfert(self, destinataire_rib, montant):
-        if montant > 0 and self.solde >= montant:
-            print(f"Transfert de {montant} DH effectué avec succès vers le compte RIB {destinataire_rib}.")
-            self.solde -= montant
-        else:
-            print("Montant de transfert non valide ou solde insuffisant.")
+        try:
+            montant = float(montant)  # Convert input to float
+            if montant > 0 and self.solde >= montant:
+                print(f"Transfert de {montant} DH effectué avec succès vers le compte RIB {destinataire_rib}.")
+                self.solde -= montant
+            else:
+                print("Montant de transfert non valide ou solde insuffisant.")
+        except ValueError:
+            print("Montant de transfert invalide. Veuillez entrer un nombre.")
 
     def afficher_solde(self):
         print(f"Solde actuel pour {self.prenom} : {self.solde} DH")
